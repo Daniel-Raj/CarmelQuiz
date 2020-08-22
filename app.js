f23c08cacea222d7a0958502a8ad3fa96e56ee93
Daniel-Raj <47554551+Daniel-Raj@users.noreply.github.com>
2020-08-22 19:37:08 +0530

    third commit

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.post("/error", function(req, res) {
     res.redirect("/quiz");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server Started");
 });
